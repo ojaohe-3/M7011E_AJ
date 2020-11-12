054fd8899b00d25a339d79835312025b9dd7fe78
ojaohe-3 <ojaohe-3@student.ltu.se>
2020-11-12 14:01:29 +0100

    Consumer group, Procumer production schema

--- Consumer-Group/Procumer.ts
+++ Consumer-Group/Procumer.ts
@@ -4,25 +4,85 @@ import {Consumer} from './consumer';
 uuidv4();
 
 class Procumer extends Consumer{
+    totalProduction : number;
+    totalCapacity: number;
+    sources: Turbine[];
+    batteries: Battery[];
+
     Procumer(turbines: Turbine[], batteries: Battery[]){
+        this.sources = turbines;
+        this.batteries = batteries;
+        this.totalProduction = 0;
+        this.totalCapacity = 0;
+        batteries.forEach((e)=>{this.totalCapacity+=e.capacity});
         
-        
+    }
+    tick(speed: number, ratio: number){
+        this.sources.forEach((turbine) => this.totalProduction += turbine.profile(speed));
+        this.batteries.forEach((b) => {
+            this.b.Input(this.totalProduction*(ratio/this.batteries.length));
+            this.totalCapacity += b.Output(1);//always output 100% power from batteries
+        });
+
     }
 }
 
 class Turbine{
     maxPower: number;//for display only
-    profile: (speed: number) => number;
+    profile: (speed: number) => number;//speed in kph
 
-   currentPower(speed: number) : number{ 
-       return this.profile(current)
-    }
-
-    Turbine(maxPower: number, profile: (power: number) => number){
-        this.profile = profile;
+    Turbine(maxPower: number){
+        this.profile = (speed) => {
+            let ratio = 0.0;
+            if(speed > 12.6 && speed < 90){
+                if(speed >= 36 && speed <= 54){
+                    ratio = 1.0;
+                }else{
+                    ratio = 0.028*speed;
+                }
+            }
+            return maxPower*ratio;
+        };
         this.maxPower = maxPower;
     }
 }
 class Battery{
+    capacity: number; // in kwh
+    current: number;
+    maxOutput: number; // maximum output in kwh
+    maxCharge: number; // maximum accepted input to chage in kwh
+    Battery(capacity: number, maxOutput: number, maxCharge: number){
+        this.capacity = capacity;
+        this.maxOutput = maxOutput;
+        this.maxCharge = maxCharge;
+        this.current = 0;
+    }
 
+    Input(power: number): number{
+        const p = power < this.maxCharge ? power : this.maxCharge;
+        this.current += p;
+        
+    
+        if(this.current < this.capacity){
+            return p;
+        }
+        else{
+            const  dp = this.current - this.capacity;
+            this.current = this.capacity;
+            return dp;
+        }
+    }
+
+    Output(ratio : number): number{
+        let power = this.maxOutput*ratio ;
+        let actual = power < this.maxOutput ? power : this.maxOutput;
+        this.current -= actual*1.1; //10% inefficency
+        if(this.current < 0){
+            this.current = 0;
+            return 0;
+        }
+        return actual;
+        
+        
+    }
 }
