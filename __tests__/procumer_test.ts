3efce80796e88182221244b3d55b3a97149779f3
ojaohe-3 <ojaohe-3@student.ltu.se>
2020-11-24 16:49:58 +0100

    manager simulation, todo api

---
+++ __tests__/procumer_test.ts
@@ -0,0 +1,33 @@
+import { Battery } from "../src/Procumer/Battery";
+import { Procumer } from "../src/Procumer/procumer";
+import { Turbine } from "../src/Procumer/Turbine";
+
+test('procumer_test',()=>{
+    let procumer = new Procumer([new Battery(1000, 51, 51)], [new Turbine(2000)]);
+    procumer.input_ratio = 1;
+    procumer.output_ratio = 0;
+    procumer.tick(3);
+    expect(procumer.totalProduction).toEqual(0);
+    for (let index = 0; index < 100; index++) {
+        procumer.tick(36);
+        
+        console.log(`batteries: ${procumer.currentCapacity()}/${procumer.totalCapacity}`)
+        console.log(`output: ${procumer.totalProduction}`)
+        if(procumer.totalCapacity === procumer.currentCapacity()){
+            procumer.input_ratio = 0;
+            procumer.output_ratio = 1;
+        }
+        if(procumer.currentCapacity() === 0)
+        {
+            procumer.input_ratio = 1;
+            procumer.output_ratio = 0;
+        }
+
+        if(procumer.input_ratio === 0)
+            expect(procumer.totalProduction).toBeGreaterThanOrEqual(2000);
+        else if(procumer.currentCapacity() !== 0)
+            expect(procumer.totalProduction).toBeLessThanOrEqual(2000);
+
+        
+    }
+});
--- src/Procumer/procumer.ts
+++ src/Procumer/procumer.ts
@@ -10,10 +10,20 @@ export class Procumer{
 
     input_ratio: number
     output_ratio: number;
+    currentCapacity: () => number;
 
     constructor(batteries: Array<Battery>, turbines: Array<Turbine>){
+        this.batteries = batteries;
+        this.turbines = turbines;
         this.totalProduction = 0;
         this.totalCapacity = 0;
+        batteries.forEach((e)=> this.totalCapacity += e.capacity)
+        this.currentCapacity = () : number=> {
+            let sum = 0;
+            batteries.forEach((e)=>sum+=e.current);
+            return sum;
+        };
+
         this.input_ratio = 0.5;
         this.output_ratio = 0.5;
     }
@@ -23,6 +33,7 @@ export class Procumer{
      * @param ratio 
      */
     tick(speed: number){
+        this.totalProduction = 0;
         this.turbines.forEach((turbine) => this.totalProduction += turbine.profile(speed));
         this.batteries.forEach((b) => {
             let tot = this.totalProduction;
