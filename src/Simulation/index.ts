2a1340740ef6973604f6c60104685933844cd247
ojaohe-3 <ojaohe-3@student.ltu.se>
2020-12-01 11:46:47 +0100

    switch

--- src/Simulation/index.ts
+++ src/Simulation/index.ts
@@ -158,8 +158,8 @@ app.post("/api/procumers",(req,res) =>{
 
 const PORT =  process.env.PORT || 5000;
 app.listen(PORT, function () {
-    console.log(`App is listening on port ${PORT}`);
-});
+    console.log(`App is listening on port ${PORT}`);``
+});0
 
 
 //todo fetch simulation data from managers
