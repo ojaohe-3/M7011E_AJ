821561967b6cc5c5351de2294decad6afd1a01e6
Albou-lab <albane.donat@gmail.com>
2020-12-02 14:28:27 +0100

    set up of vue

---
+++ src/vue_interfaces/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+
+
+
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+
+
+
+
+<style>
+  #app {
+    background-color: #34495e;
+    color: #34495e;
+  }
+
+  h3 {
+    font-size: 28px;    
+    font-family: 'Dosis', sans-serif;
+  }
+
+  h4 {
+    font-size: 24px;    
+    font-family: 'Dosis', sans-serif;
+  }
+</style>
---
+++ src/vue_interfaces/src/components/Header.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="hello">
+    <img src="../image/logo.png" alt="logo" id="logo">
+    <h1>Green Lean Electric, loggin</h1>
+    <div class="headerLine"></div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+header {
+  color: #ecf0f1;
+  display: block;
+  width: 100%;
+  margin-bottom: 50px;
+  text-align: center;
+  font-size: 38px;
+  font-weight: bold;
+  font-family: 'Dosis', sans-serif;
+}
+
+#logo {
+  height: 130px;
+  float: left;
+  position: relative;
+  top: 30px;
+  left: 30px;
+}
+
+header h1 {
+  display: inline-block;
+  position: relative;
+}
+
+#exit {
+  height: 50px;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  display: inline-block;
+}
+
+.headerLine {
+  display: block;
+  background-color: #ecf0f1;
+  height: 10px;
+  width: 65%;
+  border-radius: 25px;
+  position: absolute;
+  left: 23%;
+}
+
+@media screen and (max-width: 1150px) {
+  header {
+    font-size: 30px;
+  }
+
+  #logo {
+    height: 100px;
+  }
+
+  #exit {
+    height: 40px;
+  }
+}
+
+@media screen and (max-width: 950px) {
+  header {
+    font-size: 28px;
+  }
+
+  header h1 {
+    margin-left: 100px;
+  }
+
+  #logo {
+    height: 80px;
+  }
+
+  #exit {
+    height: 30px;
+  }
+}
+</style>
